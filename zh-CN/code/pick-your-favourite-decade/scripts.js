8b7506c7ddafa1dbcddb5474ca736b99a33ca077
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-03-17 16:15:04 +0000

    New translations scripts.js (Chinese Simplified)

--- zh-CN/code/pick-your-favourite-decade/scripts.js
+++ zh-CN/code/pick-your-favourite-decade/scripts.js
@@ -31,43 +31,16 @@ function option1() {
   instruction.style.display = 'none';
   content.style.display = 'grid';
   body.classList.add('seventies');
-  title.innerHTML = '1970s';
+  title.innerHTML = '二十世纪七十年代';
   slot1.classList.add('seventies');
   slot1.innerHTML =
-    '<h1>Fashion</h1> \n \
-          <p> \n \
-          <strong>Bohemian Chic:</strong> The 1970s saw a rise in bohemian-inspired fashion, characterised by flowing fabrics, floral prints, and earthy colors. This style was influenced by the counterculture movement and embraced a relaxed, free-spirited aesthetic. Fringed vests, peasant blouses, maxi dresses, and bell-bottom jeans were iconic elements of this trend. \n \
-          </p> \n \
-          <p> \n \
-          <strong>Disco Glamour:</strong> The disco era of the 1970s brought about glamorous and flashy fashion. Metallic fabrics, sequins, and glitter were popular, reflecting the vibrant nightlife scene. Tight-fitting clothes such as wrap dresses, jumpsuits, and platform shoes became synonymous with disco fashion. Bold colors and exaggerated silhouettes were key features of this trend. \n \
-          </p> \n \
-          <p> \n \
-          <strong>Sporty Casual:</strong> The 1970s also witnessed the emergence of sporty casual wear, influenced by the rising popularity of recreational activities like jogging and aerobics. This style featured comfortable yet stylish clothing such as tracksuits, hoodies, sneakers, and t-shirts with bold logos. The athleisure trend of mixing sportswear with everyday attire became increasingly popular during this decade. \n \
-          </p>';
+    '<h1>时尚</h1>\n           <p>\n           <strong>波西米亚风：</strong>在二十世纪七十年代，兴起了波西米亚风格的时尚潮流，其特点是飘逸的面料、碎花印花和土色。这种风格受到反主流文化运动的影响，崇尚一种轻松、自由的审美。流苏背心、农民衬衫、及地的长裙和喇叭裤是这一时尚趋势的标志性元素。\n           </p>\n           <p>\n           <strong>魅力迪斯科：</strong>在二十世纪七十年代的迪斯科时代带来了迷人而华丽的时尚风格。金属织物、亮片和闪光片很受欢迎，反映出充满活力的夜生活场景。裹身裙、连身裤和厚底鞋等紧身服装成为迪斯科时尚的代名词。大胆的色彩和夸张的轮廓是这一时尚趋势的主要特征。\n           </p>\n           <p>\n           <strong>运动休闲装：</strong>受慢跑、健美操等娱乐活动日益流行的影响，二十世纪七十年代也出现了运动休闲服。这种风格的特点是舒适而时尚的服装，如运动服、卫衣、运动鞋和带有醒目标志的t恤。在这十年中，将运动服与日常服装混合在一起的运动休闲趋势变得越来越流行。\n           </p>';
   slot2.classList.add('seventies');
   slot2.innerHTML =
-    "<h1>Music</h1> \n \
-          <p> \n \
-            <strong>Disco Fever:</strong> The 1970s were defined by the disco music craze, characterised by pulsating rhythms, funky basslines, and catchy melodies. Disco songs often featured lush orchestrations, soulful vocals, and lyrics about love, dancing, and nightlife. The disco era saw the rise of iconic artists like Donna Summer, Bee Gees, and Gloria Gaynor, as well as legendary clubs like Studio 54, where disco culture thrived. \n \
-          </p> \n \
-          <p> \n \
-            <strong>Rock 'n' Roll Evolution:</strong> Rock music underwent significant evolution in the 1970s, branching into various subgenres such as glam rock, progressive rock, and punk rock. Bands like Led Zeppelin, Pink Floyd, and The Rolling Stones pushed the boundaries of rock music with experimental sounds and complex compositions. The 1970s also saw the emergence of influential singer-songwriters like Bob Dylan and Joni Mitchell, who infused their music with introspective lyrics and folk influences. \n \
-          </p> \n \
-          <p> \n \
-            <strong>Birth of Hip-Hop:</strong> The 1970s marked the birth of hip-hop music in the Bronx, New York City, as DJs and MCs began experimenting with turntables and rhymes at block parties and community events. Hip-hop's early pioneers, such as Grandmaster Flash, Afrika Bambaataa, and DJ Kool Herc, laid the groundwork for the genre's future success. Breakdancing, graffiti art, and MC battles were integral elements of hip-hop culture during this formative decade. \n \
-          </p>";
+    '<h1>音乐</h1> \n           <p> \n             <strong>迪斯科热潮：</strong> 二十世纪七十年代的迪斯科音乐热潮以脉动的节奏、时髦的贝斯线和朗朗上口的旋律为特征。迪斯科歌曲通常以华丽的管弦乐、深情的歌声和关于爱情、舞蹈和夜生活的歌词为特色。迪斯科时代见证了唐娜·莎曼、比吉斯和葛洛莉雅·盖诺等标志性艺术家的崛起，以及 Studio 54 等传奇俱乐部的崛起，迪斯科文化在这里蓬勃发展。 \n           </p> \n           <p> \n             <strong>摇滚乐的演变：</strong> 摇滚乐在二十世纪七十年代经历了重大演变，分支出各种子流派，如华丽摇滚、前卫摇滚和朋克摇滚。齐柏林飞船、平克·弗洛伊德和滚石乐队等乐队通过实验性的声音和复杂的作曲突破了摇滚乐的界限。20 世纪 70 年代还出现了鲍勃·迪伦和琼尼·米歇尔等有影响力的创作型歌手，他们的音乐中融入了内省的歌词和民谣的影响。 \n           </p> \n           <p> \n             <strong>嘻哈音乐的诞生：</strong> 二十世纪七十年代，嘻哈音乐在纽约市布朗克斯区诞生，当时 DJ 和 MC 开始在街区派对和社区活动中尝试使用唱机和韵律。嘻哈音乐的早期先驱，如 Grandmaster Flash、Afrika Bambaataa 和 DJ Kool Herc，为该流派未来的成功奠定了基础。在这个形成时期，霹雳舞、涂鸦艺术和 MC 对决是嘻哈文化不可或缺的元素。 \n           </p>';
   slot3.classList.add('seventies');
   slot3.innerHTML =
-    "<h1>Discoveries and Advancements</h1> \n \
-          <p> \n \
-            <strong>Personal Computing:</strong> The 1970s saw the birth of the personal computer revolution, with the development of groundbreaking technologies like the microprocessor and the floppy disk. Companies like Apple, Microsoft, and IBM introduced early PCs to the market, making computing accessible to individuals and businesses alike. Innovations such as the Altair 8800 and the Apple II paved the way for the digital age, laying the foundation for the modern computer industry. \n \
-          </p> \n \
-          <p> \n \
-            <strong>Genetic Engineering:</strong> In the 1970s, scientists made significant strides in the field of genetic engineering, with the discovery of recombinant DNA technology. This breakthrough allowed researchers to manipulate and splice DNA molecules from different organisms, opening up new possibilities for biotechnology and genetic research. The creation of the first genetically modified organism (GMO) in 1973 marked a milestone in genetic engineering, sparking both excitement and ethical debates about the implications of manipulating the building blocks of life. \n \
-          </p> \n \
-          <p> \n \
-            <strong>Space Exploration:</strong> The 1970s were a golden age of space exploration, with landmark achievements such as the Apollo moon landings and the launch of the Voyager probes. NASA's Apollo program successfully put twelve astronauts on the lunar surface between 1969 and 1972, fulfilling President Kennedy's goal of landing a man on the moon before the end of the decade. The Voyager spacecraft, launched in 1977, conducted pioneering studies of the outer planets and provided invaluable data about the solar system's far reaches. \n \
-          </p>";
+    '<h1>发现和进步</h1> \n           <p> \n             <strong>个人计算：</strong> 二十世纪 7七十年代，个人计算机革命诞生，微处理器和软盘等突破性技术应运而生。苹果、微软和 IBM 等公司将早期的个人计算机推向市场，让个人和企业都能使用计算服务。Altair 8800 和 Apple II 等创新产品为数字时代铺平了道路，为现代计算机行业奠定了基础。 \n           </p> \n           <p> \n             <strong>基因工程：</strong> 二十世纪 七十年代，随着重组 DNA 技术的发现，科学家在基因工程领域取得了重大进展。这一突破使研究人员能够操纵和拼接来自不同生物体的 DNA 分子，为生物技术和基因研究开辟了新的可能性。1973 年第一个转基因生物 (GMO) 的诞生标志着基因工程的一个里程碑，引发了人们对操纵生命基本要素的影响的兴奋和伦理争论。 \n           </p> \n           <p> \n             <strong>太空探索：</strong> 二十世纪 7七十年代是太空探索的黄金时代，出现了许多里程碑式的成就，例如阿波罗登月和旅行者号探测器的发射。美国宇航局的阿波罗计划在 1969 年至 1972 年间成功将 12 名宇航员送上月球表面，实现了肯尼迪总统在 2020 年前将人类送上月球的目标。旅行者号宇宙飞船于 1977 年发射，对外行星进行了开创性的研究，并提供了有关太阳系遥远区域的宝贵数据。 \n           </p>';
   localStorage.setItem('choice', 'seventies');
 }
 
@@ -77,46 +50,16 @@ function option2() {
   instruction.style.display = 'none';
   content.style.display = 'grid';
   body.classList.add('eighties');
-  title.innerHTML = '1980s';
+  title.innerHTML = '二十世纪八十年代';
   slot1.classList.add('eighties');
   slot1.innerHTML =
-    '<h1>Fashion</h1> \n \
-          <p> \n \
-            <strong>Punk Rock Rebellion:</strong> The 1980s saw the continuation of punk fashion, characterised by rebellion and DIY aesthetics. Leather jackets adorned with studs and patches, ripped jeans, and band t-shirts were staples of the punk look. Mohawks, spiked hair, and heavy eye makeup were popular among both men and women, reflecting the subversive attitude of the punk movement. \n \
-          </p> \n \
-          <p> \n \
-              <strong>Preppy Style:</strong> Preppy fashion became mainstream in the 1980s, inspired by the collegiate look of Ivy League universities. This style featured clean-cut clothing such as button-down shirts, cable-knit sweaters, khaki pants, and plaid skirts. Pastel colors and bold stripes were commonly seen in preppy attire, along with accessories like boat shoes, pearl necklaces, and headbands. \n \
-          </p> \n \
-          <p> \n \
-              <strong>Power Dressing:</strong> The 1980s witnessed the rise of power dressing, especially among working women, who aimed to assert their authority in the workplace. Power suits with padded shoulders, tailored blazers, and high-waisted trousers became emblematic of this trend. Bold colors, geometric patterns, and statement jewelry were used to exude confidence and professionalism. \n \
-          </p> \n \
-          <p> \n \
-              <strong>New Wave Fashion:</strong> New Wave music heavily influenced fashion in the 1980s, giving rise to a futuristic and eclectic style. This aesthetic featured asymmetrical cuts, bright neon colors, and unconventional silhouettes. Fishnet stockings, leather skirts, and oversized blazers were popular items in the New Wave wardrobe. Hairstyles like the mullet and the asymmetrical bob complemented this edgy and avant-garde look. \n \
-          </p>';
+    '<h1>时尚</h1> \n           <p> \n             <strong>叛逆的朋克摇滚：</strong> 二十世纪八十年代，朋克时尚继续流行，叛逆和 DIY 美学成为其特征。饰有铆钉和补丁的皮夹克、破洞牛仔裤和乐队 T 恤是朋克造型的必备单品。莫霍克发型、尖刺发型和浓重眼妆在男女中都很流行，反映了朋克运动的颠覆性态度。 \n           </p> \n           <p> \n               <strong>学院风：</strong> 学院风在 20 世纪 80 年代成为主流，其灵感来自常春藤盟校的学院风。这种风格的服装以剪裁利落的服装为特色，例如纽扣衬衫、粗针织毛衣、卡其裤和格子裙。学院风服装中经常出现柔和的色彩和大胆的条纹，以及船鞋、珍珠项链和头带等配饰。 \n           </p> \n           <p> \n               <strong>职场强势风：</strong> 二十世纪 八十年代，强势着装风潮兴起，尤其是在职场女性中，她们希望在职场上展现自己的权威。垫肩的权力套装、剪裁合身的西装外套和高腰裤成为这一潮流的象征。大胆的色彩、几何图案和个性珠宝被用来展现自信和专业。 \n           </p> \n           <p> \n               <strong>新浪潮时尚：</strong> 新浪潮音乐对二十世纪八十年代的时尚产生了重大影响，催生了一种未来主义和折衷主义风格。这种美学以不对称剪裁、明亮的霓虹色和非传统轮廓为特色。渔网袜、皮裙和超大西装外套是新浪潮衣橱中的热门单品。马尾和不对称波波头等发型与这种前卫前卫的造型相得益彰。 \n           </p>';
   slot2.classList.add('eighties');
   slot2.innerHTML =
-    "<h1>Music</h1> \n \
-          <p> \n \
-            <strong>Pop Dominance:</strong> The 1980s were characterised by the rise of pop music as a dominant force in the mainstream. Synthesizers, drum machines, and electronic production techniques became prevalent, shaping the sound of '80s pop hits. Iconic artists like Michael Jackson, Madonna, and Prince achieved unprecedented success with their catchy melodies, flashy music videos, and larger-than-life personas. The MTV generation further propelled pop music into the spotlight, as music videos became a crucial marketing tool for artists. \n \
-          </p> \n \
-          <p> \n \
-            <strong>New Wave Revolution:</strong> New Wave music emerged in the late 1970s and reached its peak popularity in the 1980s, blending elements of punk rock, electronic music, and avant-garde art. Bands like Depeche Mode, Duran Duran, and The Cure embraced synthesizers and drum machines to create a futuristic and eclectic sound. New Wave songs often featured catchy hooks, danceable rhythms, and introspective lyrics, reflecting the angst and optimism of youth culture in the '80s. \n \
-          </p> \n \
-          <p> \n \
-            <strong>Heavy Metal Explosion:</strong> The 1980s witnessed a surge in popularity for heavy metal music, fueled by bands like Metallica, Iron Maiden, and Guns N' Roses. Known for its loud guitars, aggressive vocals, and theatrical performances, heavy metal became a cultural phenomenon in the '80s. Subgenres like thrash metal, glam metal, and hair metal attracted legions of dedicated fans, while controversial themes and imagery sparked debates about censorship and morality. \n \
-          </p>";
+    '<h1>音乐</h1> \n           <p> \n             <strong>流行音乐的主导地位：</strong> 二十世纪八十年代，流行音乐崛起，成为主流音乐的主导力量。合成器、鼓机和电子制作技术变得十分流行，塑造了八十年代流行音乐的风格。迈克尔·杰克逊、麦当娜和 Prince 等标志性艺术家凭借其朗朗上口的旋律、华丽的音乐视频和栩栩如生的形象取得了前所未有的成功。MTV 一代进一步将流行音乐推向了聚光灯下，因为音乐视频成为了艺术家的重要营销工具。 \n           </p> \n           <p> \n             <strong>新浪潮革命：</strong> 新浪潮音乐出现于二十世纪七十年代末，并在二十世纪 八十年代达到顶峰，融合了朋克摇滚、电子音乐和前卫艺术的元素。迪佩什·莫德、杜兰杜兰和 治疗乐队等采用合成器和鼓机来创造未来主义和不拘一格的声音。新浪潮歌曲通常以朗朗上口的旋律、适合跳舞的节奏和内省的歌词为特色，反映了八十年代青年文化的焦虑和乐观。 \n           </p> \n           <p> \n             <strong>重金属爆炸：</strong> 二十世纪 八十年代，重金属音乐人气飙升，金属乐队、铁娘子乐队和枪炮与玫瑰乐队等功不可没。重金属以其响亮的吉他声、激昂的歌声和戏剧表演而闻名，成为八十年代的一种文化现象。激流金属、华丽金属和重金属等子流派吸引了大批忠实粉丝，而有争议的主题和形象引发了有关审查制度和道德的争论。 \n           </p>';
   slot3.classList.add('eighties');
   slot3.innerHTML =
-    "<h1>Discoveries and Advancements</h1> \n \
-          <p> \n \
-            <strong>Personal Computing Boom:</strong> The 1980s witnessed a surge in personal computing, as technological advancements and market demand drove the adoption of PCs worldwide. The introduction of the IBM PC in 1981 set a standard for compatibility and sparked a wave of clones and competitors. Microsoft's MS-DOS operating system became ubiquitous on IBM-compatible computers, laying the groundwork for the dominance of Windows in the future. The graphical user interface (GUI) revolutionized computing with the release of Apple's Macintosh in 1984, making computers more intuitive and user-friendly. \n \
-          </p> \n \
-          <p> \n \
-            <strong>HIV/AIDS Discovery:</strong> The 1980s saw the identification of the human immunodeficiency virus (HIV) as the cause of acquired immunodeficiency syndrome (AIDS), a devastating pandemic that emerged in the early 1980s. Scientists isolated and characterized HIV in 1983, leading to improved understanding of the virus's transmission and pathogenesis. The discovery of HIV paved the way for diagnostic tests, antiretroviral therapies, and public health interventions to combat the spread of AIDS and support affected individuals. \n \
-          </p> \n \
-          <p> \n \
-            <strong>End of the Cold War:</strong> The 1980s marked the end of the Cold War, a decades-long geopolitical conflict between the United States and the Soviet Union. Diplomatic efforts like détente and arms control negotiations helped ease tensions between the superpowers, culminating in the historic events of 1989-1991. The fall of the Berlin Wall in 1989 symbolized the collapse of communism in Eastern Europe, while the dissolution of the Soviet Union in 1991 heralded the end of the bipolar world order and the beginning of a new era of global politics. \n \
-          </p>";
+    '<h1>发现和进步</h1> \n           <p> \n             <strong>个人计算热潮：</strong> 二十纪八十年代，个人计算蓬勃发展，技术进步和市场需求推动了全球个人计算机的普及。1981 年推出的 IBM PC 为兼容性树立了标准，并引发了克隆和竞争浪潮。微软的 MS-DOS 操作系统在 IBM 兼容计算机上无处不在，为 Windows 未来的主导地位奠定了基础。1984 年，随着苹果 Macintosh 的发布，图形用户界面 (GUI) 彻底改变了计算方式，使计算机更加直观和用户友好。 \n           </p> \n           <p> \n             <strong>HIV/AIDS 发现：</strong> 二十世纪八十年代，人类免疫缺陷病毒 (HIV) 被确认为获得性免疫缺陷综合征 (AIDS) 的病因，AIDS 是二十世纪八十年代初出现的一种毁灭性流行病。科学家于 1983 年分离并鉴定了 HIV，从而加深了对该病毒传播和发病机制的了解。HIV 的发现为诊断测试、抗逆转录病毒疗法和公共卫生干预铺平了道路，以对抗艾滋病的传播并支持受影响的个人。 \n           </p> \n           <p> \n             <strong>冷战的结束：</strong> 二十世纪八十年代标志着冷战的结束，冷战是美国和苏联之间长达数十年的地缘政治冲突。缓和关系和军备控制谈判等外交努力有助于缓解超级大国之间的紧张局势，最终导致了 1989-1991 年的历史性事件。1989 年柏林墙的倒塌象征着东欧共产主义的崩溃，而 1991 年苏联的解体则预示着两极世界秩序的结束和全球政治新时代的开始。 \n           </p>';
   localStorage.setItem('choice', 'eighties');
 }
 
@@ -126,7 +69,7 @@ function option3() {
   instruction.style.display = 'none';
   content.style.display = 'grid';
   body.classList.add('nineties');
-  title.innerHTML = '1990s';
+  title.innerHTML = '二十世纪九十年代';
   slot1.classList.add('nineties');
   slot1.innerHTML =
     '<h1>Fashion</h1> \n \
