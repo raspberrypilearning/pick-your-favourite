fb1166ad6e3e4ea9124a8b3b51487fddd27fe0e1
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-04-22 12:19:29 +0100

    New translations scripts.js (Chinese Simplified)

--- zh-CN/code/pick-your-favourite-football/scripts.js
+++ zh-CN/code/pick-your-favourite-football/scripts.js
@@ -38,7 +38,7 @@ function option1() {
   title.innerHTML = '利物浦足球俱乐部';
   slot1.classList.add('liverpool');
   slot1.innerHTML =
-    '<h1>最近结果</h1> \n         <p>利物浦 4 - 1 切尔西</p> \n         <p>利物浦 5 - 2 诺维奇城</p> \n         <p>富勒姆 1 - 1 利物浦</p> \n         <p>伯恩茅斯 0 - 4 利物浦</p> \n         <p>利物浦 2 - 1 富勒姆</p>';
+    '<h1>最近的结果</h1> \n         <p>利物浦 4 - 1 切尔西</p> \n         <p>利物浦 5 - 2 诺维奇城</p> \n         <p>富勒姆 1 - 1 利物浦</p> \n         <p>伯恩茅斯 0 - 4 利物浦</p> \n         <p>利物浦 2 - 1 富勒姆</p>';
   slot2.classList.add('liverpool');
   slot2.innerHTML =
     '<h1>最新消息</h1> \n         <p>尤尔根·克洛普将离开利物浦。</p>\n         <p>阿莱克西斯·麦卡利斯特可能会对阵阿森纳。</p>\n         <p>利物浦仍是夺冠热门，但仅此而已。</p>';
@@ -57,7 +57,7 @@ function option2() {
   title.innerHTML = '曼城';
   slot1.classList.add('man-city');
   slot1.innerHTML =
-    '<h1>最近的结果</h1> \n         <p>曼城 3 - 1 伯恩利</p> \n         <p>热刺 0 - 1 曼城</p> \n         <p>纽卡斯尔 2 - 3 曼城</p> \n         <p>曼城 5 - 0 哈德斯菲尔德</p> \n         <p>曼城 2 - 0 谢菲尔德联队</p>';
+    '<h1>最近的结果</h1> \n         <p>曼城 3 - 1 伯恩利</p> \n         <p>托特纳姆 0 - 1 曼城</p> \n         <p>纽卡斯尔 2 - 3 曼城</p> \n         <p>曼城 5 - 0 哈德斯菲尔德</p> \n         <p>曼城 2 - 0 谢菲尔德联队</p>';
   slot2.classList.add('man-city');
   slot2.innerHTML =
     '<h1>最新消息</h1> \n         <p>凯文·德布劳内回来了！</p>\n         <p>艾尔林·哈兰德想离开曼彻斯特加盟皇家马德里。</p>\n         <p>佩普·瓜迪奥拉正在制定离队计划，想要离开。</p>';
@@ -76,12 +76,12 @@ function option3() {
   title.innerHTML = '托特纳姆热刺';
   slot1.classList.add('tottenham');
   slot1.innerHTML =
-    '<h1>最近结果</h1> \n         <p>热刺 3 - 2 布伦特福德</p> \n         <p>热刺 0 - 1 曼城</p> \n         <p>曼联 2 - 2 热刺</p> \n         <p>热刺 1 - 0 伯恩利</p> \n         <p>热刺 3 - 1 伯恩茅斯</p>';
+    '<h1>最近结果</h1> \n         <p>托特纳姆 3 - 2 布伦特福德</p> \n         <p>托特纳姆 0 - 1 曼城</p> \n         <p>曼联 2 - 2 热刺</p> \n         <p>托特纳姆 1 - 0 伯恩利</p> \n         <p>托特纳姆 3 - 1 伯恩茅斯</p>';
   slot2.classList.add('tottenham');
   slot2.innerHTML =
     '<h1>最新消息</h1> \n         <p>卢卡斯·伯格瓦尔选择托特纳姆热刺而不是巴塞罗那。</p>\n         <p>阿尔菲·多林顿成为热刺腿筋受伤的球员之一。</p>\n         <p>热刺仍在努力寻找哈里·凯恩的替代者</p>';
   slot3.classList.add('tottenham');
-  slot3.innerHTML = '<h1>顶级球员</h1> \n         <p>孙兴慜</p>\n         <p>里查利松</p>\n         <p>佩德罗·波罗</p>';
+  slot3.innerHTML = '<h1>最佳球员</h1> \n         <p>孙兴慜</p>\n         <p>里查利松</p>\n         <p>佩德罗·波罗</p>';
   localStorage.setItem('choice', 'tottenham');
 }
 
